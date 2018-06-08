102271f7220a952f339ae5403ec57edbfed305b4
AbhiSid <abhinavsiddharth8409@gmail.com>
2018-06-08 13:42:15 +0530

    Fifth assignment

--- Lab10/p5.v
+++ Lab10/p5.v
@@ -2,6 +2,7 @@ module vat_buzzer_behavior (output buzzer,
 input above_25_0, above_30_0, low_level_0,
 input above_25_1, above_30_1, low_level_1,
 input select_vat_1 );
+  
 assign buzzer = select_vat_1 ? low_level_1 | (above_30_1 | ~above_25_1): low_level_0 | (above_30_0 | ~above_25_0);
 endmodule
 
